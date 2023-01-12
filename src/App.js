8151eff7883a0c21d40d6bcaf93133093febe0bb
AAhsanSiddiqui123 <121101608+AAhsanSiddiqui123@users.noreply.github.com>
2023-01-12 10:55:57 +0500

    todo app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+import ToDoItems from "./components/toDoItems";
+import React, {Component } from 'react';
+
+
+export default class App extends Component{
+
+  constructor(){
+    super()
+    this.state = {
+      clear: false
+    }
+  }
+
+
+  clearHandler = () =>{
+    this.setState({clear: !this.state.clear})
+  }
+
+  render(){
+
+    return (
+      
+      <div className='mainContainer'>
+      <div className="App">
+        <h1 style={{ margin: "0" }}>Todo App</h1>
+        <ToDoItems clearVal={this.state.clear} />
+      </div>
+        <div className="footer">
+          <p>Click To Erase All Todo</p>
+          <button onClick={this.clearHandler}>Clear All</button>
+        </div>
+    </div>
+  );
+}
+}
+
---
+++ src/components/List/List.js
@@ -0,0 +1,25 @@
+import React, {Component } from 'react';
+import style from "./List.module.css";
+
+
+
+export default class List extends Component {
+
+
+    deleteHandler = () => {
+        this.props.fun(this.props.id)
+    }
+
+    render() {
+
+        return (
+            <div className={style.listContainer}>
+                <li className={style.listitems}>
+                    <p>{this.props.value}</p>
+                    <button onClick={this.deleteHandler}>✖</button>
+                </li>
+            </div>
+        )
+    }
+
+}

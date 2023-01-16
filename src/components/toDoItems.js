e93a8a8c9dc3e5bcce0aad7d24910c1022b5a65b
AAhsanSiddiqui123 <121101608+AAhsanSiddiqui123@users.noreply.github.com>
2023-01-16 10:14:29 +0500

    add update featur

--- src/components/toDoItems.js
+++ src/components/toDoItems.js
@@ -20,16 +20,16 @@ export default class ToDoItems extends Component {
   submitHandler = (e) => {
     e.preventDefault();
 
-    this.setState({ isUpdate: false })
     let test = this.state.inputArray.find((curr) => {
       return curr.id === this.state.updatedId
     })
 
-    if (test && test != "") {
-      {
-        test.value = this.state.inputChange
-      }
-    } else if (this.state.inputChange != "") {
+    if (test) {
+      test.value = this.state.inputChange 
+      this.setState({ updatedId: '' })
+
+    } else if (this.state.inputChange) {
+      this.setState({ isUpdate: false })
       this.state.inputArray.push({
         value: this.state.inputChange,
         id: Math.random()
@@ -37,6 +37,7 @@ export default class ToDoItems extends Component {
     } else {
       return
     }
+    this.setState({ isUpdate: false })
     this.setState({ inputChange: "" })
   }
 
@@ -52,14 +53,16 @@ export default class ToDoItems extends Component {
 
   /////////////////////////////////////////////////////////update///////////////////////////////////////////////
 
+  toggle =  true;
   updateHandler = (id) => {
-    let newArray = this.state.inputArray.filter((curr) => {
-      return curr.id === id;
-    })
-
-    this.setState({ isUpdate: true })
-    this.setState({ updatedId: id })
-
+    if(this.toggle){
+      this.toggle = false;
+      this.setState({ updatedId: '' })
+    }else{
+      this.toggle = true;
+      this.setState({ updatedId: id })
+    }
+    this.setState({ isUpdate: this.toggle })
   }
 
 
